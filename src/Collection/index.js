924542dc7bd9a58296aef64c46cfa2e17d3d4e91
BrokenEdtz <jesenfu1001@gmail.com>
2023-04-25 14:24:20 +0700

    up

--- src/Collection/index.js
+++ src/Collection/index.js
@@ -3,6 +3,7 @@ const { Collection } = require('discord.js');
 /**
  *set afk more easy
  *@example afk.set(userId: string, [Date.now(), reason: string])
+ *afk.get(userId: string)
  */
 const afk = new Collection();
 
