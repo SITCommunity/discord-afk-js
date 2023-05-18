da2371699078ce5d80c204080b4e91561469c88e
BrokenEdtz <jesenfu1001@gmail.com>
2023-05-18 16:42:52 +0700

    final fixing update notifier

--- src/Collection/afk.ts
+++ src/Collection/afk.ts
@@ -1,21 +1,22 @@
 import { Collection } from '@discordjs/collection';
-import { default as searchForNewUpdate } from '../function/version';
+import updateNotifier from 'update-notifier';
+import pkgjs from '../../package.json' assert { type: 'json' };
 
 /**
- *discord-afk-js package for more easy to make afk command without db (database)
- *@example
- *```js
- *afk.set(userId: string, [Date.now(), reason: string])
- *afk.get(userId: string)
- *```
+ * discord-afk-js package for more easy to make afk command without db (database)
+ *
+ * click this for example about {@link https://github.com/CyraTeam/discord-afk-js/#readme | discord-afk-js} package
  */
-const afk = new Collection();
+const afk = new Collection<any, any>();
 
 /**
  * The {@link https://github.com/CyraTeam/discord-afk-js/#readme | discord-afk-js} version
  * that you are currently using.
  */
 declare const version: string;
-searchForNewUpdate({ state: true });
+const notifier = updateNotifier({ pkg: pkgjs, updateCheckInterval: 1000 * 60 * 60 * 24 })
+notifier.notify();
+console.log(notifier.update);
 
 export { afk, version };
+//# sourceMappingURL=afk.ts.map
