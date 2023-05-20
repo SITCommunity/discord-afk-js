06c57becf56ab275b98de774a653ff992ca3c0c4
BrokenEdtz <jesenfu1001@gmail.com>
2023-05-20 21:21:48 +0700

    update to 1.4.3

---
+++ src/index.d.ts
@@ -0,0 +1,2 @@
+export { afk, version } from "./main/afk";
+//# sourceMappingURL=index.d.ts.map
--- src/main/afk.ts
+++ src/main/afk.ts
@@ -1,20 +1,24 @@
-import { Collection } from '@discordjs/collection';
-import updateNotifier from 'update-notifier';
-import pkgjs from '../../package.json' assert { type: 'json' };
+import { default as updateNotifier } from 'update-notifier';
+import packages from '../../package.json' assert { type: 'json' };
+import { AfkCollection } from '../../dist/main/afkcollection';
 
+//afk collection
 /**
  * discord-afk-js package for more easy to make afk command without db (database)
  *
  * click this for example about {@link https://github.com/CyraTeam/discord-afk-js/#readme | discord-afk-js} package
  */
-const afk = new Collection<any, any>();
+const afk = new AfkCollection<any, any>();
 
+//version of discord-afk-js package
 /**
  * The {@link https://github.com/CyraTeam/discord-afk-js/#readme | discord-afk-js} version
  * that you are currently using.
  */
 declare const version: string;
-const notifier = updateNotifier({ pkg: pkgjs, updateCheckInterval: 1000 * 60 * 60 * 24 })
+
+//notifier update package
+const notifier = updateNotifier({ pkg: packages, updateCheckInterval: 1000 * 60 * 60 * 24 })
 notifier.notify();
 console.log(notifier.update);
 
