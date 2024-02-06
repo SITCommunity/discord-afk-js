05e815799524072c0f03909e6bba1cecf4825e21
brokenedtzjs <jesenfu1001@gmail.com>
2024-02-06 17:56:37 +0700

    update

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 const { AfkClient, versions } = require('../dist');
 const afk = new AfkClient();
+test("add function", async () => {
+    afk.connect({ token: 'mongodb+srv://Discord:quit75gamingpro@bot.zobkl.mongodb.net/rickastley' });
 
-afk.connect({ token: 'mongodb+srv://Discord:quit75gamingpro@bot.zobkl.mongodb.net/rickastley' });
-//afk.addUser({ id: '123456' });
-//const user = afk.findUser('123456');
-//console.log(user);
-const reason = afk.findMessage('123456');
-console.log(reason);
-//afk.removeUser('123456');
+    afk.addUser({ id: '123456' });
+
+    const user = await afk.findUser('123456');
+    console.log(user);
+
+    const reason = await afk.findMessage('123456');
+    console.log(reason);
+
+    afk.removeUser('123456');
+});
